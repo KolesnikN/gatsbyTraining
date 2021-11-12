c879b224bee72306f0e8a3dc96ec8798b652fff4
nick <givemeblood57@gmail.com>
2021-11-12 08:29:23 +0300

    connect gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,18 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "First site with gatsby",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
+// plugins: [
+//   "gatsby-source-contentful",
+//   "gatsby-plugin-image",
+//   "gatsby-plugin-sharp"
+//   options: {
+//   apiKey: process.env.API_KEY,
+//       password: process.env.PASSWORD,
+//       url: process.env.url
+// }
+// ],
